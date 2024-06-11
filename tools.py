6cd6d27ee78d2a795597b26f24df07c34efdd4f3
RhFz1 <nooralifirst@gmail.com>
2024-06-11 10:57:31 +0530

    Fix Bugs and Typos

--- tools.py
+++ tools.py
@@ -2,9 +2,9 @@ import os
 import csv
 from typing import Optional
 from dateutil import parser
-from langchain.agents import tools
+from langchain.agents import tool
 
-@tools
+@tool
 def read_csv(input_path: str) -> str:
     """This functions read a CSV file from the given input path and returns the string representation of the file"""
     with open(input_path, 'r') as file:
@@ -13,11 +13,11 @@ def read_csv(input_path: str) -> str:
         for row in reader:
             csv_data += ','.join(row) + '\n'
     return csv_data
-@tools
+@tool
 def extract_dates(text: str):
     """This function takes in a relevant text body which contain a single date context and returns a date object, in case of multiple dates try to run the function multiple times only with the relevant body of text which contains a single date"""
     return parser.parse(text).date()
-@tools
+@tool
 def send_mail(text: Optional[str], subject: Optional[str], recipient: Optional[str], cc: Optional[str]) -> str:
     """This function sends an email to the recipient with the given subject and text"""
     return f"Email has been sent to {recipient} with subject: {subject} and text: {text}"
