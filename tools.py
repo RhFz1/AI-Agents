b650409efe292f3584ea910dec314dc4c8fb5973
RhFz1 <nooralifirst@gmail.com>
2024-06-11 15:13:28 +0530

    Add tool

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import os
 import csv
 from typing import Optional
+from datetime import datetime
 from dateutil import parser
 from langchain.agents import tool
 
@@ -18,9 +19,13 @@ def extract_dates(text: str):
     """This function takes in a relevant text body which contain a single date context and returns a date object, in case of multiple dates try to run the function multiple times only with the relevant body of text which contains a single date"""
     return parser.parse(text).date()
 @tool
+def convert_to_time(text: str) -> datetime.time:
+    """This function takes in a relevant text body which contain a single time context and returns a time object, in case of multiple times try to run the function multiple times only with the relevant body of text which contains a single time"""
+    return datetime.strptime(text, "%H:%M:%S").time()
+@tool
 def send_mail(text: Optional[str], subject: Optional[str], recipient: Optional[str], cc: Optional[str]) -> str:
     """This function sends an email to the recipient with the given subject and text"""
     return f"Email has been sent to {recipient} with subject: {subject} and text: {text}"
 
 
-mail_tools = [read_csv, extract_dates, send_mail]
+mail_tools = [read_csv, extract_dates, send_mail, convert_to_time]
