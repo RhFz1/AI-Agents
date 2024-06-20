559db90032bc898a7252eec7ed3423eb3a116ef2
RhFz1 <nooralifirst@gmail.com>
2024-06-20 17:01:30 +0530

    Add notification logic

--- mtools.py
+++ mtools.py
@@ -1,24 +1,44 @@
-from typing import Annotated
-
+import os
+import csv
+from io import StringIO
+from typing import Annotated, Optional
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.tools import tool
 from langchain_experimental import repl
 
-tavily_tool = TavilySearchResults(max_results=5)
-
 # Warning: This executes code locally, which can be unsafe when not sandboxed
 
 @tool
-def python_repl(
-    code: Annotated[str, "The python code to execute to generate your chart."],
-):
-    """Use this to execute python code. If you want to see the output of a value,
-    you should print it out with `print(...)`. This is visible to the user."""
-    try:
-        result = repl.run(code)
-    except BaseException as e:
-        return f"Failed to execute. Error: {repr(e)}"
-    result_str = f"Successfully executed:\n```python\n{code}\n```\nStdout: {result}"
-    return (
-        result_str + "\n\nIf you have completed all tasks, respond with FINAL ANSWER."
-    )
+def read_db_for_roster(path: str) -> Optional[__file__]:
+    """This function reads a database which contains data regarding the availabilities of the personnel i.e., fetching the roster of personnel."""
+
+    # lets assume here we are making an API call to the database which fetches the roster data for the current day.
+    # for now, lets assume the API call returns a csv file.
+    # lets try to read the contents of the file and save it temporarily.
+    temp = __file__
+    csv_data = StringIO(temp)
+
+    csv_string_data = ''
+    csv_reader = csv.reader(csv_data)
+
+    for row in csv_reader:
+        csv_string_data += ','.join(row) + '\n'
+    
+    return csv_string_data
+
+def notification_logic(num: int) -> bool:
+    pass
+
+@tool
+def notify_roster_personnel(id: list, message: str) -> str:
+    """This function expects a list of tuples which is (mobile number, first name) in str format of all the available personnel according to their today's roster, and a """
+
+    if len(id) == 0:
+        return "No personnel available today."
+    
+    sent = []
+
+    for num, name in id:
+        if notification_logic(num):
+            sent.append(name)
+    return ' '.join(sent) + " have been notified."
