2b87fb4524b68a7e702b0184337e7a819bd9bf91
RhFz1 <nooralifirst@gmail.com>
2024-06-21 09:50:43 +0530

    Add Graph Constructor for adding multiagent settup

---
+++ graph.py
@@ -0,0 +1,100 @@
+import functools
+import operator
+from typing import Sequence, TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langgraph.graph import END, StateGraph
+from multiagent import create_agent, agent_node
+from langchain_core.output_parsers.openai_functions import JsonOutputFunctionsParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_openai import ChatOpenAI
+from mtools import read_db_for_roster, notify_roster_personnel
+
+members = ["Database Reader", "Notifier"]
+system_prompt = (
+    "You are a supervisor tasked with managing a conversation between the"
+    " following workers:  {members}. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH."
+)
+# Our team supervisor is an LLM node. It just picks the next agent to process
+# and decides when the work is completed
+options = ["FINISH"] + members
+# Using openai function calling can make output parsing easier for us
+function_def = {
+    "name": "route",
+    "description": "Select the next role.",
+    "parameters": {
+        "title": "routeSchema",
+        "type": "object",
+        "properties": {
+            "next": {
+                "title": "Next",
+                "anyOf": [
+                    {"enum": options},
+                ],
+            }
+        },
+        "required": ["next"],
+    },
+}
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder(variable_name="messages"),
+        (
+            "system",
+            "Given the conversation above, who should act next?"
+            " Or should we FINISH? Select one of: {options}",
+        ),
+    ]
+).partial(options=str(options), members=", ".join(members))
+
+llm = ChatOpenAI(model="gpt-4")
+
+supervisor_chain = (
+    prompt
+    | llm.bind_functions(functions=[function_def], function_call="route")
+    | JsonOutputFunctionsParser()
+)
+
+# The agent state is the input to each node in the graph
+class AgentState(TypedDict):
+    # The annotation tells the graph that new messages will always
+    # be added to the current states
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+    # The 'next' field indicates where to route to next
+    next: str
+
+database_reader_agent = create_agent(llm, 
+                                     [read_db_for_roster], 
+                                     "You are a database reader. You will read a database and return the data.")
+database_reader_agent_node = functools.partial(agent_node, agent=database_reader_agent, name="Database Reader")
+
+# NOTE: THIS PERFORMS ARBITRARY CODE EXECUTION. PROCEED WITH CAUTION
+notifier_agent = create_agent(
+    llm,
+    [notify_roster_personnel],
+    "You may generate safe python code to analyze data and generate charts using matplotlib.",
+)
+notifier_agent_node = functools.partial(agent_node, agent=notifier_agent, name="Coder")
+
+workflow = StateGraph(AgentState)
+workflow.add_node("Database Reader", database_reader_agent_node)
+workflow.add_node("Coder", notifier_agent_node)
+workflow.add_node("supervisor", supervisor_chain)
+
+# this block is for adding links in the graph.
+for member in members:
+    # We want our workers to ALWAYS "report back" to the supervisor when done
+    workflow.add_edge(member, "supervisor")
+# The supervisor populates the "next" field in the graph state
+# which routes to a node or finishes
+conditional_map = {k: k for k in members}
+conditional_map["FINISH"] = END
+workflow.add_conditional_edges("supervisor", lambda x: x["next"], conditional_map)
+# Finally, add entrypoint
+workflow.set_entry_point("supervisor")
+
+graph = workflow.compile()
